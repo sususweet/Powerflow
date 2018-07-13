4d2b196f0d72d4ff407f5b10d2a8c42ed73c0a8e
sususweet <tyq9702@sina.com>
2018-07-13 17:05:03 +0800

    Add gaussion method

--- gassion_sader.m
+++ gassion_sader.m
@@ -1,6 +1,6 @@
 clear;
 
-max1=100;
+max1=1000;
 eps1=1.0e-10;
 eps2=1.0e-10;
 
@@ -25,7 +25,7 @@ global Q;
 %节点重新编号开始
 [bus, line] = rearrange(bus, line);
 %节点重新编号结束
-filename = ['.\output_', dfile, '.dat'];
+filename = ['.\output_gassion_', dfile, '.dat'];
 myf=fopen(filename,'w');
 
 Y= generateY(bus,line);
@@ -55,44 +55,74 @@ for count=1:max1
     fprintf(myf, '\n');
     fprintf(myf, '--------------第%d次迭代的结果----------\n', count);
     
-    [deltaP,deltaQ] = dPQ(Y,bus);
-    J=form_jac(Y,bus);
+    U_new=zeros(nPoint,1);
+    theta_new=zeros(nPoint,1);
     
-    fprintf(myf, '-------------第%d次迭代的雅可比矩阵J----------\n', count);
-    [count_i, count_j] = size(J);
-    for i=1:count_i
-        for k=1:count_j
-            fprintf(myf, '%11.6f  ', J(i,k));
+    %采用同步迭代
+    %首先处理PQ节点
+    for i = 1:nPQ
+        Ui = U(i,1) * exp(j*theta(i,1));
+        sumYU = 0;
+        for k = 1:nPoint
+            if k < i
+                sumYU = sumYU + Y(i,k)*U_new(k,1) * exp(j*theta(k,1));
+            elseif k > i
+                sumYU = sumYU + Y(i,k)*U(k,1) * exp(j*theta(k,1));
+            end
         end
-        fprintf(myf, '\n');
+        Ui_new = ((P(i,1) - j*Q(i,1))/conj(Ui) - sumYU) / Y(i,i);
+        
+        U_new(i,1) = abs(Ui_new);
+        theta_new(i,1) = angle(Ui_new);
     end
+    %PQ节点处理完毕
     
-    fprintf(myf, '\n');
-    fprintf(myf, '-------------第%d次迭代的功率偏差dP和dQ----------\n', count);
-    for i=1: length(deltaP)
-        fprintf(myf, 'dP%d   %13.6e\n', i, deltaP(i,1));
+    %PV节点处理开始
+    for i = nPQ +1:nPQ+nPV
+        Ui = U(i,1) * exp(j*theta(i,1));
+        sumYU = 0;
+        for k = 1:nPoint
+            sumYU = sumYU + conj(Y(i,k))*conj(U(k,1) * exp(j*theta(k,1)));
+        end
+        Qp_new = imag(Ui*sumYU);
+        
+        Ui = U(i,1) * exp(j*theta(i,1));
+        sumYU = 0;
+        for k = 1:nPoint
+            if k ~= i
+                sumYU = sumYU + Y(i,k)*U(k,1) * exp(j*theta(k,1));
+            end
+        end
+        Ui_new = ((P(i,1) - j*Qp_new)/conj(Ui) - sumYU) / Y(i,i);
+        
+        %只对相角进行修正
+        U_new(i,1) = U(i,1);
+        theta_new(i,1) = angle(Ui_new);
     end
-    for i=1: length(deltaQ)
-        fprintf(myf, 'dQ%d   %13.6e\n', i, deltaQ(i,1));
+    %PV节点处理完毕
+    
+    %SW节点处理开始
+    for i = nPQ+nPV +1:nPoint
+        U_new(i,1) = U(i,1);
+        theta_new(i,1) = theta(i,1);
     end
+    %SW节点处理结束
     
-    deltaPQ=[deltaP;deltaQ];
-    deltaUtheta = J^(-1)*deltaPQ;
-    
-    deltatheta = deltaUtheta(1:nPoint-nSW,:);
-    deltaU = deltaUtheta(nPoint:nPoint+nPQ-nSW,:);
-    
+    %计算误差值
+    deltaU = U_new - U;
+    deltatheta = theta_new - theta;
+
     fprintf(myf, '\n');
     fprintf(myf, '-------------第%d次迭代的节点相角和电压的偏差dx----------\n', count);
     for i=1: length(deltatheta)
         fprintf(myf, 'dang%d   %13.6e\n', i, deltatheta(i,1));
     end
     for i=1: length(deltaU)
-        fprintf(myf, 'dU%d/U%d     %13.6e\n', i, i, deltaU(i,1));
+        fprintf(myf, 'dU%d     %13.6e\n', i, deltaU(i,1));
     end
     
-    theta(1:nPoint-nSW,1)=theta(1:nPoint-nSW,1) - deltatheta;
-    U(1:nPQ,1)=U(1:nPQ,1) - deltaU .* U(1:nPQ,1) ;
+    U = U_new;
+    theta = theta_new;
     
     fprintf(myf, '\n');
     fprintf(myf, '-------------第%d次迭代的节点相角delta(弧度为单位）和电压U----------\n', count);
@@ -111,7 +141,7 @@ end
 [bus_new_result, line_result] = calculate_result (Y, bus, line);
 
 fprintf(myf, '\n');
-fprintf(myf, '---------------牛顿－拉夫逊法潮流计算结果----------\n');
+fprintf(myf, '---------------高斯 - 塞德尔法潮流计算结果----------\n');
 fprintf(myf, ' 节点计算结果：\n');
 fprintf(myf, '节点   节点电压    节点相角（角度）    节点注入功率\n');
 
